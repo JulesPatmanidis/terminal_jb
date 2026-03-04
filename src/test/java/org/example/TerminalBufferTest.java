1e751b71d4bc32665f66703c84c2027bf500556e
JulesPatmanidis <ipat@hotmail.gr>
2026-03-04 11:18:01 +0100

    Test: add more test cases for some edge cases

--- src/test/java/org/example/TerminalBufferTest.java
+++ src/test/java/org/example/TerminalBufferTest.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -25,6 +28,16 @@ class TerminalBufferTest {
     }
 
     @Test
+    void moveCursorWrapsAcrossMultipleRowsAndColumns() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
+
+        buffer.moveCursor(13, 7);
+
+        assertEquals(3, buffer.getCursorPos().x());
+        assertEquals(0, buffer.getCursorPos().y());
+    }
+
+    @Test
     void writeTextHandlesNewLine() {
         TerminalBuffer buffer = new TerminalBuffer(10, 3, 10);
 
@@ -34,6 +47,19 @@ class TerminalBufferTest {
         assertScreenLineEquals(buffer, 1, "world     ");
         assertEquals(5, buffer.getCursorPos().x());
         assertEquals(1, buffer.getCursorPos().y());
+    }
+
+    @Test
+    void writeTextHandlesConsecutiveNewlines() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 4, 10);
+
+        buffer.writeText("A\n\nB");
+
+        assertScreenLineEquals(buffer, 0, "A    ");
+        assertScreenLineEquals(buffer, 1, "     ");
+        assertScreenLineEquals(buffer, 2, "B    ");
+        assertEquals(1, buffer.getCursorPos().x());
+        assertEquals(2, buffer.getCursorPos().y());
     }
 
     @Test
@@ -117,16 +143,63 @@ class TerminalBufferTest {
     }
 
     @Test
+    void scrollbackRespectsConfiguredMaximumSize() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 1, 2);
+
+        buffer.setCursorPos(0, 0);
+        buffer.writeText("1111");
+        buffer.insertEmptyLine();
+        buffer.setCursorPos(0, 0);
+        buffer.writeText("2222");
+        buffer.insertEmptyLine();
+        buffer.setCursorPos(0, 0);
+        buffer.writeText("3333");
+        buffer.insertEmptyLine();
+
+        assertEquals("2222 ", buffer.getScrollbackLineAt(0));
+        assertEquals("3333 ", buffer.getScrollbackLineAt(1));
+        assertEquals("", buffer.getScrollbackLineAt(2));
+    }
+
+    @Test
     void attributesAreStoredAndReturned() {
         TerminalBuffer buffer = new TerminalBuffer(5, 2, 10);
-        TextAttributes attrs = new TextAttributes(TerminalBuffer.RED, TerminalBuffer.BG_BLUE, TerminalBuffer.BOLD);
+        TextAttributes attrs = new TextAttributes(TextFGColor.FG_RED, TextBGColor.BG_BLUE, Set.of(TextStyle.BOLD));
         buffer.setAttributes(attrs);
 
         assertEquals(attrs, buffer.getAttributes());
         buffer.writeText("Q");
         assertEquals(attrs, buffer.getScreenAttributesAt(0, 0));
-        TextAttributes defaults = new TextAttributes(TerminalBuffer.WHITE, TerminalBuffer.BG_BLACK, "");
+        TextAttributes defaults = new TextAttributes(TextFGColor.FG_DEFAULT, TextBGColor.BG_DEFAULT, Set.of());
         assertEquals(defaults, buffer.getScreenAttributesAt(-1, 0));
+    }
+
+    @Test
+    void writeTextAppliesForegroundBackgroundAndStyleToWrittenCell() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 2, 10);
+        buffer.setForeground(TextFGColor.FG_GREEN);
+        buffer.setBackground(TextBGColor.BG_RED);
+        buffer.setStyles(Set.of(TextStyle.UNDERLINE));
+
+        buffer.writeText("X");
+
+        TextAttributes expected = new TextAttributes(TextFGColor.FG_GREEN, TextBGColor.BG_RED, Set.of(TextStyle.UNDERLINE));
+        assertEquals(expected, buffer.getScreenAttributesAt(0, 0));
+    }
+
+    @Test
+    void setStylesUsesDefensiveCopyAndIgnoresNullInput() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 2, 10);
+        HashSet<TextStyle> mutableStyles = new HashSet<>();
+        mutableStyles.add(TextStyle.BOLD);
+
+        buffer.setStyles(mutableStyles);
+        mutableStyles.add(TextStyle.UNDERLINE);
+
+        assertEquals(Set.of(TextStyle.BOLD), buffer.getStyles());
+
+        buffer.setStyles(null);
+        assertEquals(Set.of(TextStyle.BOLD), buffer.getStyles());
     }
 
     @Test
@@ -144,11 +217,11 @@ class TerminalBufferTest {
     @Test
     void getScrollbackAttributesAtReturnsStoredAndDefaultValues() {
         TerminalBuffer buffer = new TerminalBuffer(5, 2, 10);
-        TextAttributes attrs = new TextAttributes(TerminalBuffer.GREEN, TerminalBuffer.BG_WHITE, TerminalBuffer.UNDERLINE);
+        TextAttributes attrs = new TextAttributes(TextFGColor.FG_GREEN, TextBGColor.BG_WHITE, Set.of(TextStyle.UNDERLINE));
         buffer.setAttributes(attrs);
         buffer.writeText("A\nB\nC");
 
-        TextAttributes defaults = new TextAttributes(TerminalBuffer.WHITE, TerminalBuffer.BG_BLACK, "");
+        TextAttributes defaults = new TextAttributes(TextFGColor.FG_DEFAULT, TextBGColor.BG_DEFAULT, Set.of());
         assertEquals(attrs, buffer.getScrollbackAttributesAt(0, 0));
         assertEquals(defaults, buffer.getScrollbackAttributesAt(1, 0));
         assertEquals(defaults, buffer.getScrollbackAttributesAt(0, 1));
@@ -169,6 +242,14 @@ class TerminalBufferTest {
         TerminalBuffer buffer = new TerminalBuffer(5, 2, 10);
         buffer.writeText("AAAAA\nBBBBB\nCC");
         assertEquals("AAAAA\nBBBBB\nCC   \n", buffer.getScreenAndScrollbackContent());
+    }
+
+    @Test
+    void getScreenLineAtReturnsNullForOutOfBounds() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 2, 10);
+
+        assertEquals(null, buffer.getScreenLineAt(-1));
+        assertEquals(null, buffer.getScreenLineAt(2));
     }
 
     private static void assertScreenLineEquals(TerminalBuffer buffer, int y, String expected) {
